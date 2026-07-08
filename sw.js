1cdabdaac747c8bbc1c56e1ed53ce2348c373867
Davi Romauski Meurer <davi.meurer@intranet.ciss>
2026-07-08 09:19:03 -0300

    Adiciona manifest e service worker para habilitar PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'kanban-v1-cache';
+const urlsToCache = [
+  './Kanban.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
